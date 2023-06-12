eff4b75b7074679ea92b81d47c206195e0904726
pazalmironnicolas <pazalmironnicolas@chaco.gob.ar>
2023-06-12 10:05:54 -0300

    cambios menores

--- main.py
+++ main.py
@@ -1,27 +1,77 @@
-import db
-from fastapi import FastAPI,HTTPException, Body
+from fastapi import FastAPI, HTTPException, Body
 
 app = FastAPI()
 app.title = "Api de Nico"
 app.version = "1.0"
 
 
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+                "rating": 7.8,
+                "category": "Acción"
+    },
 
-@app.get("/movies/{id}",tags=["Movies"])
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+                "rating": 7.8,
+                "category": "Acción"
+    }
+]
+
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+
+@app.get("/movies/{id}", tags=["Movies"])
 def get_movie(id: int):
-    for item in db.data:
+    for item in movies:
         if item["id"] == id:
             return item
-    raise HTTPException(status_code=404, detail="Pelicula no encontrada") 
+    raise HTTPException(status_code=404, detail="Pelicula no encontrada")
 
 
-@app.get("/movies/", tags=["Movies"])
-def get_movies_by_year(year: str):
-    for item in db.data:
-        if item["year"] == year:
-            return item
-        
-@app.post("/movies",tags=["Movies"])
-def create_movies(id: int = Body(), title: str = Body(), year: str = Body(), runtime:int= Body()):
-    return title
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str, year: int):
+    return [item for item in movies if item['category'] == category]
 
+
+@app.post('/movies', tags=['movies'])
+def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies
+
+
+@app.put('/movies/{id}', tags=['movies'])
+def update_movie(id: int, title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    for item in movies:
+        if item['id'] == id:
+               item['title'] == title
+               item['overview'] == overview
+               item['year'] == year
+               item['rating'] == rating
+               item['category'] == category
+        return movies
+
+
+@app.delete('/movies/{id}', tags=['movies'])
+def delete_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            movies.remove(item)
+            return movies
